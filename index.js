93da7ac79504ddd5382e38d633cb51c8e0964f08
Manzar Hassan <manzarh@sysotel.ai>
2023-04-06 19:40:37 +0530

    Fix: fixes svg exports

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ import NoImageFound from "./assets/MarketChannelLogos/NO_IMAGE_FOUND.svg"
 
 export const MarketChannelLogo = (marketChannel) => {
     if (marketChannel in MarketChannelMapping) {
-        return `url(data:image/svg+xml;base64,${btoa(MarketChannelMapping[marketChannel])})`;
+        return MarketChannelMapping[marketChannel]
 
     } else {
-        return `url(data:image/svg+xml;base64,${btoa(NoImageFound)})`;
+        return NoImageFound
 
     }
 }
