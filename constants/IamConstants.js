223978819207cf2f3997be9e8ad74acfcfb14c4a
Manzar Hassan <manzarh@sysotel.ai>
2023-05-20 13:44:24 +0530

    add: adds Iya and Iam constants

---
+++ constants/IamConstants.js
@@ -0,0 +1,5 @@
+export const MODULE_ID_CMS = 'CMS';
+export const MODULE_ID_BOOKING_ENGINE = 'IBE';
+export const MODULE_ID_CHANNEL_MANAGER = 'CM';
+export const MODULE_ID_RATE_SHOPPER = 'RS';
+export const MODULE_ID_REVIEW_MINDER = 'RM';
