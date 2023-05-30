6fbee498e1d699ce5193d234cfb18e3c9ce091f2
Manzar Hassan <manzarh@sysotel.ai>
2023-05-30 15:38:43 +0530

    adds iya constants and delete channelConstants file

--- constants/ChannelConstants.js
+++
@@ -1,7 +0,0 @@
-export const SYSOTEL_AI_IYA = 'SYSOTEL_AI_IYA';
-export const RMS_REST_API = 'RMS_REST_API';
-export const RESAVENUE_CM_API = 'RESAVENUE_CM_API';
-export const RATE_GAIN_DEMAND_AI = 'RATE_GAIN_DEMAND_AI';
-export const AI_RATE_METRICS = 'AI_RATE_METRICS';
-export const AI_REVIEWS = 'AI_REVIEWS';
-export const AI_EDDI = 'AI_EDDI';
