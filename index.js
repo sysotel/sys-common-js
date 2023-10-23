d772deb537c7b81ceb5d86b5628de422dfd0c511
Manzar Hassan <manzarh@sysotel.ai>
2023-10-23 15:51:47 +0530

    adds amenityMappings

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import {LogoMappings} from "./mappings/MarketChannelLogosMapping/LogoMappings.js";
 import NoImageFound from './assets/MarketChannelLogos/NO_IMAGE_FOUND.svg'
+import iconMapper from "./mappings/AmenityIconsMapping/IconMappings.js";
 
 export const MarketChannelLogo = (marketChannel) => {
     if (marketChannel in LogoMappings) {
@@ -9,4 +10,15 @@ export const MarketChannelLogo = (marketChannel) => {
         return NoImageFound
 
     }
+}
+
+export const AmenityIcon = (amenity) => {
+    const requiredAmenity = iconMapper(amenity)
+
+    if(requiredAmenity) {
+        return requiredAmenity
+
+    }
+
+    return 'DoNotDisturbAlt'
 }
