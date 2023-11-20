5f3b162be54f7a1064d0630b2b1190a10390db0b
Manzar Hassan <manzarh@sysotel.ai>
2023-11-21 00:12:37 +0530

    improves code

--- svgMappings/PaymentGatewayIconsMapping.js
+++ svgMappings/PaymentGatewayIconsMapping.js
@@ -2,26 +2,19 @@ const {
     PAYMENT_GATEWAY_PAYU,
     PAYMENT_GATEWAY_RAZORPAY,
     PAYMENT_GATEWAY_AIRPAY,
-    PAYMENT_GATEWAY_CASHFREE
+    PAYMENT_GATEWAY_CASHFREE,
 } = require("../constants/BeConstants");
 
 const paymentGatewayIcon = (paymentGateway) => {
-
     const paymentGatewayIconsMapping = {
-        [PAYMENT_GATEWAY_PAYU]: require('../assets/MarketChannelLogos/payu.svg').default,
-        [PAYMENT_GATEWAY_RAZORPAY]: require('../assets/MarketChannelLogos/Razorpay.svg').default,
-        [PAYMENT_GATEWAY_AIRPAY]: require('../assets/MarketChannelLogos/airpay.svg').default,
-        [PAYMENT_GATEWAY_CASHFREE]: require('../assets/MarketChannelLogos/Cashfree.svg').default,
-        'NO_IMAGE_FOUND': require('../assets/MarketChannelLogos/NO_IMAGE_FOUND.svg').default,
-    }
+        [PAYMENT_GATEWAY_PAYU]: require("../assets/MarketChannelLogos/payu.svg"),
+        [PAYMENT_GATEWAY_RAZORPAY]: require("../assets/MarketChannelLogos/Razorpay.svg"),
+        [PAYMENT_GATEWAY_AIRPAY]: require("../assets/MarketChannelLogos/airpay.svg"),
+        [PAYMENT_GATEWAY_CASHFREE]: require("../assets/MarketChannelLogos/Cashfree.svg"),
+        "NO_IMAGE_FOUND": require("../assets/MarketChannelLogos/NO_IMAGE_FOUND.svg"),
+    };
 
-    if (paymentGateway in paymentGatewayIconsMapping) {
-        return paymentGatewayIconsMapping[paymentGateway]
+    return paymentGatewayIconsMapping[paymentGateway] || paymentGatewayIconsMapping["NO_IMAGE_FOUND"];
+};
 
-    } else {
-        return paymentGatewayIconsMapping['NO_IMAGE_FOUND']
-
-    }
-}
-
-module.exports = paymentGatewayIcon
+module.exports = paymentGatewayIcon;
